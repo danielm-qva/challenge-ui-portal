8114c353deaf3cb66cd2885c5a0b7c344156782e
Daniel Mastrapa <jmastrapa96@gmail.com>
2025-08-16 22:07:44 -0400

    [feature] build fix, for ISR.

--- src/app/(public)/categories/[id]/page.tsx
+++ src/app/(public)/categories/[id]/page.tsx
@@ -5,16 +5,16 @@ import { categoriesData } from '../../../../../mock/categories';
 export const revalidate = 3600;
 
 export async function generateStaticParams() {
-  // const categories = await fetch(`/api/categories`).then((res) => res.json());
   return categoriesData?.slice(0, 3).map((cat: ICategory) => ({ id: cat.id?.toString() }));
 }
 
 export default async function CategoryPage({ params }: PropsParams) {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
+  // const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
   const { id } = await params;
-  const category = await fetch(`${baseUrl}/api/categories/${id}`).then((res) => res.json());
+  const category = categoriesData.find((cat) => cat.id.toString() === id);
+  // const category = await fetch(`${baseUrl}/api/categories/${id}`).then((res) => res.json());
 
-  if (!category?.data) return notFound();
+  if (!category) return notFound();
 
   return (
     <div className="w-full bg-white py-8">
@@ -22,11 +22,11 @@ export default async function CategoryPage({ params }: PropsParams) {
         <div className="mb-8 grid grid-cols-1 gap-6 md:grid-cols-3">
           <div className="rounded-xl bg-yellow-50 p-6">
             <h3 className="mb-2 text-lg font-semibold text-yellow-800">ID de Categoría</h3>
-            <p className="text-3xl font-bold text-yellow-600">{category?.data?.id}</p>
+            <p className="text-3xl font-bold text-yellow-600">{category?.id}</p>
           </div>
           <div className="rounded-xl bg-yellow-50 p-6">
             <h3 className="mb-2 text-lg font-semibold text-yellow-800">Nombre de Categoría</h3>
-            <p className="text-3xl font-bold text-yellow-600">{category?.data?.name}</p>
+            <p className="text-3xl font-bold text-yellow-600">{category?.name}</p>
           </div>
         </div>
       </div>
