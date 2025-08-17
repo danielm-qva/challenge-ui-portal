164ad6da80a42b37668303d0d8910764a881d400
Daniel Mastrapa <jmastrapa96@gmail.com>
2025-08-17 13:16:00 -0400

    [hotfix] update card nedd.

--- src/components/NeddYou/card-nedd.tsx
+++ src/components/NeddYou/card-nedd.tsx
@@ -30,7 +30,7 @@ const CardNedd = ({ title, description, keyId }: Props) => {
         style={{
           fontSize: '14px',
           fontWeight: 400,
-          fontStyle: 'normal',
+          color: '#979ea9',
           lineHeight: 'normal'
         }}
       >
